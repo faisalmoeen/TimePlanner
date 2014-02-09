80545fadd06e96902cbced8819e8d439b2be2ec3
tschlufter <t.schlufter@gmail.com>
2014-02-09 20:33:52 +0100

    fixed errors in IOptimization

--- TimePlanner/JavaSource/de/tuberlin/hdis14/core/IOptimization.java
+++ TimePlanner/JavaSource/de/tuberlin/hdis14/core/IOptimization.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Map;
 
 import de.tuberlin.hdis14.cinema.Cinema;
+import de.tuberlin.hdis14.restaurant.CinemaRestaurant;
 import de.tuberlin.hdis14.restaurant.Restaurant;
 
 public interface IOptimization {
@@ -13,5 +14,5 @@ public interface IOptimization {
 	 * @param cinemaRestList All valid combinations of cinema and restaurant
 	 * @return Map of 3 optimal combinations of cinema and restaurant 
 	 */
-	public static Map<Cinema, Restaurant> getOptimalCombination(List<CinemaRestaurant> cinemaRestList);
+	public Map<Cinema, Restaurant> getOptimalCombination(List<CinemaRestaurant> cinemaRestList);
 }
